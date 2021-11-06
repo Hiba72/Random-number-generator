a75e38eb9a6db40d4fa6e059a0ab04e95daadb19
Hiba Saqib <hibasaqib72@gmail.com>
2021-11-06 14:10:26 +0500

    random number is successfully generated

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,14 +2,32 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View;
+import android.widget.Button;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    int rand;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        Button btn=findViewById(R.id.clickButton);
+        TextView randomText=findViewById(R.id.randomNumber);
+
+        Random random=new Random();
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             rand=random.nextInt(100);
+             randomText.setText(String.valueOf(rand));
+            }
+        });
+
     }
 }
